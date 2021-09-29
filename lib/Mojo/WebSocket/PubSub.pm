33cf0d81e81371fc493f38fa75d8159ff8714e1e
EmilianoBruni <info@ebruni.it>
2021-09-29 14:24:22 +0200

    First import

---
+++ lib/Mojo/WebSocket/PubSub.pm
@@ -0,0 +1,49 @@
+package Mojo::WebSocket::PubSub;
+
+use strict;
+use warnings;
+
+1;
+
+
+__END__
+
+# ABSTRACT: descrizione del modulo
+
+=pod
+
+=encoding UTF-8
+
+=begin :badge
+
+=begin html
+
+<p>
+    <a href="https://github.com/emilianobruni/mojo-websocket-pubsub/actions/workflows/test.yml">
+        <img alt="github workflow tests" src="https://github.com/emilianobruni/mojo-websocket-pubsub/actions/workflows/test.yml/badge.svg">
+    </a>
+    <img alt="Top language: " src="https://img.shields.io/github/languages/top/emilianobruni/mojo-websocket-pubsub">
+    <img alt="github last commit" src="https://img.shields.io/github/last-commit/emilianobruni/mojo-websocket-pubsub">
+</p>
+
+=end html
+
+=end :badge
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS/CONTRIBUTING
+
+Please report any bugs through the web interface at L<https://github.com/EmilianoBruni/mojo-websocket-pubsub/issues>
+If you want to contribute changes or otherwise involve yourself in development, feel free to fork the Git repository from
+L<https://github.com/EmilianoBruni/mojo-websocket-pubsub/>.
+
+=head1 SUPPORT
+
+You can find this documentation with the perldoc command too.
+
+    perldoc Mojo::WebSocket::PubSub
+
+=cut
